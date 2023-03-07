603a39557884d6001f6ce3f5e2bddd051749e9ac
aewsunantha <sunantha1881@gmail.com>
2023-03-07 21:54:42 +0700

    reviewUpdate

--- Assets/Scenes/Developer/Scrum/RVcontrol.cs
+++ Assets/Scenes/Developer/Scrum/RVcontrol.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RVcontrol : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject i1,i2,i3,i4;
+    public GameObject i1,i2,i3,i4,i5;
+    public GameObject pm,po,sm,all;
+    public Button yourButton;
+    public int click;
     public void im1()
     {
         i1.SetActive(true);
@@ -27,20 +31,81 @@ public class RVcontrol : MonoBehaviour
        
         i4.SetActive(true);
     }
+       public void im5()
+    {
+       
+        i5.SetActive(true);
+    }     
+   
+    public void PM()
+    {
+        pm.SetActive(true);
+        po.SetActive(false);
+        sm.SetActive(false);
+    }
+    public void PO()
+    {
 
+        po.SetActive(true);
+        pm.SetActive(false);
+        sm.SetActive(false);
+
+    }
+    public void SM()
+    {
+
+        sm.SetActive(true);
+        po.SetActive(false);
+        pm.SetActive(false);
+    }
 
 
     void Start()
     {
-        Invoke("im1", 3f);
-        Invoke("im2", 5f);
-        Invoke("im3", 7f);
-        Invoke("im4", 9f);
+        click = 1;
+        Invoke("im1", 1f);
+        Invoke("im2", 2f);
+        Invoke("im3", 3f);
+        Invoke("im4", 4f);
+        Invoke("im5", 5f); 
+        Invoke("PM",  6f); 
+   
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+
+
+    }
+    void TaskOnClick()
+    {
+        click++;
+     if(click == 2)
+        {
+            po.SetActive(true);
+            pm.SetActive(false);
+            sm.SetActive(false);
+
+        }else if(click == 3)
+        {
+            sm.SetActive(true);
+            po.SetActive(false);
+            pm.SetActive(false);
+        }else if(click == 4)
+        {
+            sm.SetActive(false);
+            po.SetActive(false);
+            pm.SetActive(false);
+            all.SetActive(true);
+        }
+        else
+        {
+
+        }
+      
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+
+
 }
